f0673477ce226553342e958c24df671df3b5f0c9
Ashish Raj <ashishraj.96@gmail.com>
2026-08-19 01:59:03 +0530

    Configure staging/marts materializations, override stg_payments to table

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,24 +1,26 @@
-with
+-- stg_customers.sql 
 
-source as (
 
-    select * from {{ source('jaffle_shop', 'customers') }}
+with 
 
-),
+source as ( 
+    select * from {{ source('jaffle_shop','customers')}}
 
-renamed as (
+), 
 
-    select
+renamed as ( 
+
+    select 
 
     ------id
-    id as customer_id,
+    id as customer_id, 
+
 
     ------strings
-    first_name as first_name,
+    first_name as first_name, 
     last_name as last_name
 
     from source
-
 )
 
 select * from renamed
--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='table') }}
+
 with 
 
 source as ( select * from {{ source('jaffle_shop','payments')}}
